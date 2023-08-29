9cde0bfd64b9513dbf4356bd5065c3fa2b51e910
Narawit Sariddeechaikul <narawitpun@gmail.com>
2023-08-30 01:08:49 +0700

    Rename variables and interface

--- src/components/MyCoursesCard.tsx
+++ src/components/MyCoursesCard.tsx
@@ -1,29 +1,29 @@
-interface MyCourseCard {
+interface MyCoursesCard {
   courseThumbnailUrl: string;
   courseName: string;
   instructorName: string;
   percentCompleted: number;
 }
 
-export default function MyCoursesCard(myCourseCard: MyCourseCard) {
+export default function MyCoursesCard(myCoursesCard: MyCoursesCard) {
   return (
     <div className="card w-[249px] h-[304px] bg-white drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)] rounded-[20px]">
       <img
         className="object-cover h-[146px] rounded-t-[20px]"
-        src={myCourseCard.courseThumbnailUrl}
+        src={myCoursesCard.courseThumbnailUrl}
         alt="Course Thumbnail Picture"
       />
       <div className="px-3 pt-2 text-[14px] font-bold">
-        {myCourseCard.courseName}
+        {myCoursesCard.courseName}
       </div>
       <div className="px-3 pt-1 text-[12px] text-gray-400 font-semibold col">
-        {myCourseCard.instructorName}
+        {myCoursesCard.instructorName}
       </div>
-      <div className="px-3 pt-3 text-[12px] font-semibold">{`สำเร็จไปแล้ว ${myCourseCard.percentCompleted}%`}</div>
+      <div className="px-3 pt-3 text-[12px] font-semibold">{`สำเร็จไปแล้ว ${myCoursesCard.percentCompleted}%`}</div>
       <div className="px-3">
         <progress
           className="progress"
-          value={`${myCourseCard.percentCompleted}`}
+          value={`${myCoursesCard.percentCompleted}`}
           max="100"
         ></progress>
       </div>

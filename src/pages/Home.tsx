6bc5054cbbc4ec08d6cf562acdb515c19a616c33
Nhuengzii <32nhueng@gmail.com>
2023-08-31 22:43:09 +0700

    update: add Carousel to Home page
    add two ProgramCarousel with mock data

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,40 @@
+import ProgramCarousel from "../features/stores/components/ProgramCarousel";
+
+function mockPrograms(num: number) {
+    const programs = []
+    const mockInstructor = [
+        "อาจารย์ อนันต์ สุขสวัสดิ์",
+        "อาจารย์ สมชาย สุขสวัสดิ์",
+        "อาจารย์ สมหญิง สุขสวัสดิ์",
+        "mrs Jiraporn",
+        "mr. Jirapong",
+    ]
+    for (let i = 0; i < num; i++) {
+        programs.push({
+            courseName: `คอร์สเรียนที่ ${i + 1}`,
+            instructorName: mockInstructor[i % mockInstructor.length],
+            percentCompleted: Math.floor(Math.random() * 100),
+            courseThumbnailUrl: `https://picsum.photos/${i}/${300}`,
+        })
+    }
+    return programs
+}
+
 export default function Home() {
     return (
-        <div>Home</div>
+        <div>
+            <div className="ml-6">
+                <ProgramCarousel programs={mockPrograms(20)}
+                    carouselName="คอร์สเรียนยอดนิยม"
+                    displayCount={5}
+                />
+            </div>
+            <div className="ml-6 mt-5">
+                <ProgramCarousel programs={mockPrograms(20)}
+                    carouselName="คลาสเรียนใหม่ล่าสุด"
+                    displayCount={5}
+                />
+            </div>
+        </div>
     )
 }

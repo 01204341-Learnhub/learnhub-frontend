6d28a9d8e660d2ca8b850aedecf54a5a7be45ef3
Nhuengzii <32nhueng@gmail.com>
2023-08-31 10:23:04 +0700

    fix: add redux Provider to root component

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './index.css'
 import NavbarLayout from './layouts/NavbarLayout'
@@ -7,18 +8,21 @@ import Home from './pages/Home'
 import Landing from './pages/Landing'
 import Login from './pages/Login'
 import Register from './pages/Register'
+import store from './store'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter >
-      <Routes >
-        <Route path='/' element={<Landing />} />
-        <Route element={<NavbarLayout />}>
-          <Route path='/home' element={<Home />} />
-        </Route>
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter >
+        <Routes >
+          <Route path='/' element={<Landing />} />
+          <Route element={<NavbarLayout />}>
+            <Route path='/home' element={<Home />} />
+          </Route>
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 )

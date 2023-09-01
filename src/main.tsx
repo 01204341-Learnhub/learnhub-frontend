acbab23c52820c7831832373dbacc0d1029a9a07
Nhuengzii <32nhueng@gmail.com>
2023-09-01 23:34:14 +0700

    create: teaching zone
    TeacherMainbar, TeacherNavbarLayout, add some route

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './index.css'
 import MainbarLayout from './layouts/MainbarLayout'
 import StudentNavbarLayout from './layouts/StudentNavbarLayout'
+import TeacherNavbarLayout from './layouts/TeacherNavbarLayout'
 import CourseDetail from './pages/CourseDetail'
 import Home from './pages/Home'
 import Landing from './pages/Landing'
@@ -15,6 +16,9 @@ import LearningCourses from './pages/students/LearningCourses'
 import LearningHomeworks from './pages/students/LearningHomeworks'
 import LearningOverview from './pages/students/LearningOverview'
 import LearningSchedule from './pages/students/LearningSchedule'
+import TeachingClasses from './pages/teachers/TeachingClasses'
+import TeachingCourses from './pages/teachers/TeachingCourses'
+import TeachingHomeworks from './pages/teachers/TeachingHomeworks'
 import store from './store'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -32,6 +36,11 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
               <Route path='/learn/classes' element={<LearningClasses />} />
               <Route path='/learn/homework' element={<LearningHomeworks />} />
               <Route path='/learn/schedule' element={<LearningSchedule />} />
+            </Route>
+            <Route element={<TeacherNavbarLayout />}>
+              <Route path='/teach/courses' element={<TeachingCourses />} />
+              <Route path='/teach/classes' element={<TeachingClasses />} />
+              <Route path='/teach/homework' element={<TeachingHomeworks />} />
             </Route>
           </Route>
           <Route path='/login' element={<Login />} />

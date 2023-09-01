eba1c10d9dfea7cffc6fd7fec19038bb74157c5a
Nhuengzii <32nhueng@gmail.com>
2023-09-01 21:21:57 +0700

    create & implement: CourseDetail page
    also create some component, servicer that using in this page
    and also have a lot of hardcode.

--- src/features/stores/services/courses.ts
+++ src/features/stores/services/courses.ts
@@ -1,5 +1,28 @@
+import { Course } from "../types/course";
+
 async function listCourse() {
-    return []
+  return [];
 }
 
-export { listCourse }
+async function getCourse(id: string): Promise<Course> {
+  return {
+    id: id,
+    name: "What is Sakuya doing?",
+    description:
+      "Sakuya Izayoi (十六夜 咲夜, Izayoi Sakuya) is the Chief Maid who serves Remilia Scarlet, the head of the Scarlet Devil Mansion. She is also apparently the only human living within the mansion grounds. She is implied to be a vampire hunter like her mistress, but it is unknown if she is a human or a vampire. She is known for her ability to manipulate time.",
+    price: 100,
+    cover:
+      "https://rare-gallery.com/livewalls/imgpreview/96504-Sakuya-Izayoi.jpg",
+    rating: 4.5,
+    reviewerCount: 1222,
+    intructor: {
+      id: id,
+      name: "Supaporn Panyayai",
+      avatarUrl:
+        "https://images-na.ssl-images-amazon.com/images/S/pv-target-images/ae4816cade1a5b7f29787d0b89610132c72c7747041481c6619b9cc3302c0101._RI_TTW_.jpg",
+      jobTitle: "Software Engineer",
+    },
+  };
+}
+
+export { getCourse, listCourse };
---
+++ src/pages/CourseDetail.tsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import CourseDetailedSummary from "../features/stores/components/CourseDetailedSummary"
+import ProgramCoverWithInstructorProfile from "../features/stores/components/ProgramCoverWithInstructorProfile"
+import { getCourse } from "../features/stores/services/courses"
+import { Course } from "../features/stores/types/course"
+
+function CourseDetail() {
+    const { id } = useParams()
+    const [course, setCourse] = useState<Course | null>(null)
+    useEffect(() => {
+        async function fetchCourseDetail() {
+            const course = await getCourse(id!)
+            setCourse(course)
+        }
+        fetchCourseDetail()
+    }, [])
+    if (course === null) {
+        return null;
+    }
+    return (
+        <div className="w-full grid grid-cols-3">
+            <div className="col-span-2" >
+                <div>
+                    <span className="text-2xl font-bold">คอร์สเรียน</span>
+                    <span className="mx-2 text-2xl font-bold text-gray-500">{course.name}</span>
+                </div>
+                <ProgramCoverWithInstructorProfile programCoverUrl={course.cover}
+                    instructor={{ profileUrl: course.intructor.avatarUrl, name: course.intructor.name, jobTitle: course.intructor.jobTitle }}
+                    reviewCount={course.reviewerCount}
+                    rating={course.rating} />
+                <h1 className="text-5xl font-bold mt-3">{course.name}</h1>
+                <p className="mt-3">{course.description}</p>
+            </div>
+            <div className="flex justify-center mt-10">
+                <CourseDetailedSummary costs={4000}
+                    quantity={10}
+                    level="Beginner"
+                    students={100}
+                    availablesource={10000}
+                    hours={10}
+                    examples={200}
+                    status="Available"
+                />
+            </div>
+        </div>
+    )
+}
+
+export default CourseDetail

d3a8a2fa6c01fa1369863b7f3a83f8cb2b34ac39
Peerasu <fecownp@gmail.com>
2023-09-04 14:32:35 +0700

    fix: change from pixel to % ROUND1

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -12,19 +12,19 @@ export default function Register() {
   return (
     <>
         <div className=" flex-row">
-            <button className=" ml-4 py-3 text-[24px]" onClick={handleBack}>ย้อนกลับ</button>
+            <button className=" ml-[1%]  text-[24px]" onClick={handleBack}>ย้อนกลับ</button>
         </div>
 
         <div className=" flex-row">
-            <p className="mb-4 text-center text-[40px] font-normal">
+            <p className=" text-center text-[40px] font-normal">
           สร้างบัญชีผู้ใช้สำหรับ
             </p>
         </div>
 
         <div className="grid grid-cols-2">
-            <div className="text-center">
-                <img src={studentImage} className="w-full h-[80%] opacity-20 relative z-0"/>
-                <div className=" absolute z-50 bottom-64">
+            <div className="text-center relative">
+                <img src={studentImage} className="w-full h-[95%] opacity-20 z-0"/>
+                <div className=" absolute z-50 bottom-[40%] ml-[10%]">
                     <h1 className=" mb-[5%] text-[32px] text-center font-bold">ผู้เรียน</h1>
                     <h1 className=" ml-[25%] text-left text-[20px] font-bold">
                     สำหรับผู้ใช้ที่เข้าแพลทฟอร์มมาเพื่อหาความรู้ในเรื่องต่างๆ<br></br>
@@ -32,13 +32,13 @@ export default function Register() {
                     ไม่สามารถได้รับรายได้จากแพลทฟอร์มนี้ได้
                     </h1>
                     <div className=" mt-[40%]">
-                        <Link to="/Login" className=" text-white bg-[#007399] p-2 rounded-md shadow-md text-[24px]">สร้างบัญชีผู้เรียน</Link>
+                        <Link to="/Login" className=" text-white bg-[#007399] p-[2%] rounded-md shadow-md text-[24px]">สร้างบัญชีผู้เรียน</Link>
                     </div>
                 </div>
             </div>
-            <div className="text-center">
-                <img src={teacherImage} className="w-full h-[80%] opacity-20 relative z-0"/>
-                <div className=" absolute z-50 bottom-64 ">
+            <div className="text-center relative">
+                <img src={teacherImage} className="w-full h-[95%] opacity-20 z-0"/>
+                <div className=" absolute z-50 bottom-[40%] ml-[10%]">
                     <h1 className=" mb-[5%] text-[32px] text-center font-bold">ผู้สอน</h1>
                     <p className=" ml-[25%] text-left text-[20px] font-bold">
                     ผู้ใช้ที่เข้าแพลทฟอร์มมาเพื่อเปิดห้องเรียนสร้างและเผยแพร่สื่อการสอน<br></br>
@@ -46,13 +46,13 @@ export default function Register() {
                     สามารถหารายได้จากแพลทฟอร์มนี้ได้
                     </p>
                     <div className=" mt-[40%]">
-                        <Link to="/Login" className=" text-white bg-[#007399] p-2 rounded-md shadow-md text-[24px]">สร้างบัญชีผู้สอน</Link>
+                        <Link to="/Login" className=" text-white bg-[#007399] p-[2%] rounded-md shadow-md text-[24px]">สร้างบัญชีผู้สอน</Link>
                     </div>
                 </div>
             </div>
         </div>
 
-        <div className=" mt-5 flex flex-row justify-center space-x-2 text-[24px]">
+        <div className="flex flex-row justify-center space-x-2 text-[24px]">
             <h3 className=" font-semibold">มีบัญชีเเล้ว?</h3>
             <Link to="/Login" className=" font-bold text-blue-600">คลิกเพื่อลงชื่อเข้าสู่ระบบ</Link>
         </div>  

74e69c4e2dd082bf753a89987b0083a62e48f775
gitrinz99 <mightyrinz@gmail.com>
2023-09-09 03:24:50 +0700

    MainBar add user drop down

--- src/components/MainBar.tsx
+++ src/components/MainBar.tsx
@@ -1,12 +1,20 @@
-import { faBell, faBook, faCartShopping, faHeart, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { faBell, faBook, faBookmark, faCartShopping, faHeart, faMagnifyingGlass, faRightFromBracket } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import booklogo from '../assets/Images/bookLogo.png'
 import namelogo from '../assets/Images/textNameLogo.png'
 import mockprofile from '../assets/Images/regStudent.png'
+import { useState } from 'react'
 
 function MainBar() {
+
+    const [isDropdownOpen, setDropdownOpen] = useState(false)
+
+    const toggleDropdown = () => {
+    setDropdownOpen(!isDropdownOpen)
+    }
+
     return (
-        <div style={{height: '10vh'}} className='fixed bg-white flex w-screen items-center py-5'>
+        <nav style={{height: '10vh'}} className='fixed bg-white flex w-screen items-center py-5'>
             <div className='flex flex-row items-center justify-center w-1/12'>
                 <img className=' w-2/5' src={booklogo} alt="booklogo" />
             </div>
@@ -26,10 +34,66 @@ function MainBar() {
                 <FontAwesomeIcon icon={faCartShopping} size='xl' />
                 <FontAwesomeIcon icon={faBell} size='xl' />
             </div>
-            <div className='  w-[55px] h-[55px] mr-3 justify-self-center' >
+            <button
+                className="w-[50px] h-[50px] mr-3 justify-self-center"
+                id="user-menu-button"
+                aria-expanded={isDropdownOpen}
+                onClick={toggleDropdown}
+            >
                 <img src={mockprofile} alt="profile" className=' h-full rounded-full' />
+            </button>
+            <div
+            className={`${
+                isDropdownOpen ? 'block' : 'hidden' }
+                absolute top-[70px] right-[70px] mt-2 w-[320px] h-[550px] bg-white border border-gray-300 rounded-lg shadow divide-y divide-gray-100`}
+                id="user-dropdown"
+            >
+                <div className="px-8 py-3">
+                    <img src={mockprofile} alt="profile" className=' h-[50px] w-[50px] rounded-full' />
+                    <span className="block text-[18px] font-bold text-black">Barameehee</span>
+                    <span className="block text-[16px]  text-gray-500 truncate ">barabara@mikelab.com</span>
+                </div>
+                <ul className="py-2" aria-labelledby="user-menu-button">
+                    <li>
+                        <a  className="block px-8 py-2 text-[18px] font-medium text-black hover:bg-gray-100">หน้าแรก</a>
+                    </li>
+                    <li>
+                        <a  className="block px-8 py-2 text-[18px] font-medium text-black hover:bg-gray-100 ">การเรียนรู้ของฉัน
+                            <FontAwesomeIcon icon={faBook} size='xl' color='#068FFF' className='mx-3'/>
+                        </a>
+                    </li>
+                    <li>
+                        <a  className="block px-8 py-2 text-[18px] font-medium text-black hover:bg-gray-100 ">หลักสูตรที่อยากได้
+                            <FontAwesomeIcon icon={faBookmark} size='xl' color='#F1C93B' className='mx-3'/>
+                        </a>
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                        <a  className=" block px-8 py-2 text-[18px] font-medium text-black hover:bg-gray-100">ตั้งค่าบัญชี</a>
+                    </li>
+                    <li>
+                        <a  className=" block px-8 py-2 text-[18px] font-medium text-black hover:bg-gray-100">แก้ไขโปรไฟล์</a>
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                        <a  className=" block px-8 py-2 text-[18px] font-medium text-black hover:bg-gray-100">ประวัติการซื้อ</a>
+                    </li>
+                    <li>
+                        <a  className=" block px-8 py-2 text-[18px] font-medium text-black hover:bg-gray-100">วิธีการชำระเงิน</a>
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                        <a  className=" absolute bottom-0 block w-full px-8 py-2 text-[18px] font-medium text-black hover:bg-gray-100">ออกจากระบบ
+                            <FontAwesomeIcon icon={faRightFromBracket} size='xl' className='mx-3'/>
+                        </a>
+   
+                    </li>
+                </ul>
             </div>
-        </div>
+        </nav>
     )
 }
 

3b812de6d35fda63f673dce944c7d6d82fbe4b70
gitrinz99 <mightyrinz@gmail.com>
2023-09-08 11:44:15 +0700

    studentregister input boxes

--- src/pages/StudentsRegister.tsx
+++ src/pages/StudentsRegister.tsx
@@ -6,39 +6,57 @@ function StudentsRegister() {
 
 
     return (
-    <div className=" h-screen">
+    <div className=" bg-gray-300 h-screen">
         <div className="grid grid-cols-2">
             <div className=" ">
                 <Link to="/Register" className="text-[24px]">ย้อนกลับ</Link>
                 <p className="text-[24px] text-center  mt-[100px]  font-bold">**text**</p>
             </div>
 
-            <div className="bg-gray-700 h-screen flex flex-col justify-center items-center">
+            <div className="bg-white h-screen flex flex-col justify-center items-center">
                 <div className="">
-                    <h1 className=" mb-[50px] text-center text-[32px] font-normal">สร้างบัญชีผู้เรียน</h1>
+                    <h1 className=" mb-[50px] text-center text-black text-[32px] font-normal">สร้างบัญชีผู้เรียน</h1>
                 </div>
                 <div className=" flex border-[2px] rounded-xl space-x-10 object-contain h-[53px] w-[494px] items-center">
-                    <div className=" opacity-50 flex space-x-10 ml-4">
+                    <div className=" opacity-50 flex space-x-10 ml-4 w-full">
                         <FontAwesomeIcon icon={faEnvelope} size='2xl'></FontAwesomeIcon>
-                        <p className=" text-[24px]">Username</p>
+                        <input
+                            type="text"
+                            className="border-none outline-none text-[24px] w-full bg-white"
+                            placeholder="Username / ชื่อผู้ใช้งาน"
+                        />
                     </div>
+                    
                 </div>
+                
                 <div className=" mt-10 flex border-[2px] rounded-xl space-x-10 object-contain h-[53px] w-[494px] items-center">
-                    <div className=" opacity-50 flex space-x-10 ml-4">
+                    <div className=" opacity-50 flex space-x-10 ml-4 w-full">
                         <FontAwesomeIcon icon={faEnvelope} size='2xl'></FontAwesomeIcon>
-                        <p className=" text-[24px]">อีเมลล์</p>
+                        <input
+                            type="email"
+                            className="border-none outline-none text-[24px] w-full"
+                            placeholder="Email / อีเมล"
+                            />
                     </div>
                 </div>
                 <div className=" mt-10 flex border-[2px] rounded-xl space-x-10 object-contain h-[53px] w-[494px] items-center">
-                    <div className=" opacity-50 flex space-x-10 ml-4">
+                    <div className=" opacity-50 flex space-x-10 ml-4 w-full">
                         <FontAwesomeIcon icon={faUnlockKeyhole} size='2xl'></FontAwesomeIcon>
-                        <p className=" text-[24px]">รหัสผ่าน</p>
+                        <input
+                            type="password"
+                            className="border-none outline-none text-[24px] w-full "
+                            placeholder="Password / รหัสผ่าน"
+                        />
                     </div>
                 </div>
                 <div className=" mt-10 flex border-[2px] rounded-xl space-x-10 object-contain h-[53px] w-[494px] items-center">
-                    <div className=" opacity-50 flex space-x-10 ml-4">
+                    <div className=" opacity-50 flex space-x-10 ml-4 w-full">
                         <FontAwesomeIcon icon={faUnlockKeyhole} size='2xl'></FontAwesomeIcon>
-                        <p className=" text-[24px]">ยืนยันรหัสผ่าน</p>
+                        <input
+                            type="password"
+                            className="border-none outline-none text-[24px] w-full "
+                            placeholder="Confirm Password / ยืนยันรหัสผ่าน"
+                        />
                     </div>
                 </div>
                

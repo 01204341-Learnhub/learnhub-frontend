b3be344654e29f2b9b7ec8b2caaacfdab43694b2
peebaKU <baramee.254450@gmail.com>
2023-09-18 14:54:18 +0700

    fix: fix position

--- src/pages/students/LearningOverview.tsx
+++ src/pages/students/LearningOverview.tsx
@@ -1,6 +1,22 @@
+import ClassTeachNow from "../../features/learns/components/ClassTeachNow"
+
 function LearningOverview() {
     return (
-        <h1>current class</h1>
+        <>
+        <div className="flex">
+            <div className="basis-3/4 bg-pink-300">
+            <div>การเรียนรู้ของฉัน</div>
+            <div>คลาสเรียนที่กำลังสอนแล้ว</div>
+            <div >
+                <ClassTeachNow ThumbnailUrl={"https://miro.medium.com/v2/resize:fit:1400/0*mMBXJ9daWzStSCt5.jpg"} TitleName={"Principal Redstone Designs"} Minute={0} ></ClassTeachNow>
+            </div>
+            </div>
+            <div className="basis-1/4 bg-green-500">
+            calculator
+            </div>
+            
+        </div>
+        </>
     )
 }
 

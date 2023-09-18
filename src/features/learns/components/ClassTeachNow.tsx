995e07380ecf223d2024b6151b04b150aac8a0d5
peebaKU <baramee.254450@gmail.com>
2023-09-18 20:59:51 +0700

    fix : fix position

--- src/features/learns/components/ClassTeachNow.tsx
+++ src/features/learns/components/ClassTeachNow.tsx
@@ -6,25 +6,26 @@ import { Link } from "react-router-dom";
 
 interface ClassTeachNowProp {
     ThumbnailUrl: string;
-    ClassName: string;
+    TitleName: string;
     Minute: number; 
 }
 
 export default function ClassTeachNow (props:ClassTeachNowProp) {
 
     return (
-        <div className="relative">
-            <img src={props.ThumbnailUrl} alt="Thumbnail Picture" className=" object-cover w-[680px] h-[220px] opacity-30 rounded-xl"/>
-            <div className="absolute -translate-y-[250%] translate-x-[5%]">
-                <div className="text-5xl font-bold">{props.ClassName}</div>
+    <div className="relative w-[80%] h-[220px]">
+        <div className="static">
+            <img src={props.ThumbnailUrl} alt="Thumbnail Picture" className=" object-cover w-[100%] h-[220px] opacity-70 rounded-xl hover:opacity-50" />
+            <div className="absolute top-2 left-4">
+                <div className="text-5xl font-bold ">{props.TitleName}</div>
                 <div className=" flex space-x-2 items-center">
                     <div className="text-[20px] font-bold translate-y-[40%] bg-[#ADE792] rounded-xl p-1">กำลังสอน</div>
                     <FontAwesomeIcon icon={faClockRotateLeft} className=" translate-y-[60%]" size="xl"></FontAwesomeIcon>
                     <div className=" text-[20px] font-bold translate-y-[50%]">เริ่มสอนไปแล้ว {props.Minute} นาที</div>
                 </div>
             </div>
-            <Link to="/Login" className=" absolute -translate-y-[140%] translate-x-[300%] bg-black text-white p-1 rounded-xl text-[20px] font-bold">ไปยังคลาสเรียนนี้</Link>
-            
         </div>
+        <Link to="/Login" className=" absolute bottom-2 right-4 bg-black text-white p-1 rounded-xl text-[20px] font-bold">ไปยังคลาสเรียนนี้</Link>
+    </div>
     )
 }

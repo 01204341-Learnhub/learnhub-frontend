7d9040340a5338f4999ce9736d579c85e73f32f0
peebaKU <baramee.254450@gmail.com>
2023-09-20 10:13:30 +0700

    add: add checkbox

--- src/pages/component/AnnulmentDropdown.tsx
+++ src/pages/component/AnnulmentDropdown.tsx
@@ -10,14 +10,15 @@ export default function AnnulmentDropdown(props:AnnulmentDropdownProp) {
     const [show,setShow] = useState(false)
     return (
         <>
-        <div>
+        <div className='mx-[10%]'>
             <div className="flex bg-red-500 justify-between">
-                <div>{props.title}</div>
+                <div className='text-xl'>{props.title}</div>
                 <button onClick={()=>setShow(!show)}>show</button>
             </div>
             {show && props.works.map((item)=>(
-            <li key={item.id}>
+            <li key={item.id} className='flex justify-between mx-[5%]'>
                 <h1>{item.name}</h1>
+                <input type="checkbox"></input>
             </li>    
             ))
                 

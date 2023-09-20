abda52f09d24f73b369126e1d755ab41c393337f
peebaKU <baramee.254450@gmail.com>
2023-09-20 10:01:35 +0700

    add: add props

--- src/pages/component/AnnulmentDropdown.tsx
+++ src/pages/component/AnnulmentDropdown.tsx
@@ -1,18 +1,21 @@
-
 import { useState } from 'react';
 
-export default function AnnulmentDropdown() {
-    const works = [{id:1,name:"ก้อง"},{id:2,name:"โจโจ้"},{id:3,name:"โดโด้"}]
-    const title = "บูมคนหล่อ"
+
+interface AnnulmentDropdownProp {
+    title: string;
+    works: Array<any>
+}
+
+export default function AnnulmentDropdown(props:AnnulmentDropdownProp) {
     const [show,setShow] = useState(false)
     return (
         <>
         <div>
             <div className="flex bg-red-500 justify-between">
-                <div>{title}</div>
+                <div>{props.title}</div>
                 <button onClick={()=>setShow(!show)}>show</button>
             </div>
-            {show && works.map((item)=>(
+            {show && props.works.map((item)=>(
             <li key={item.id}>
                 <h1>{item.name}</h1>
             </li>    

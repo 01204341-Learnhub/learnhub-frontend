b8a591f4c4a540038953a108884e44ebd8776f4f
peebaKU <baramee.254450@gmail.com>
2023-09-23 15:57:16 +0700

    fix : fix name component

--- src/pages/students/LearningClasses.tsx
+++ src/pages/students/LearningClasses.tsx
@@ -1,4 +1,4 @@
-import AnnulmentDropdown from  "../component/AnnouncementDropdown"
+import AnnouncementDropdown from  "../component/AnnouncementDropdown"
 function LearningClasses() {
     const courses = [{title:"บทที่ 1: แนะนำผู้สอน ”บูม ” ",Studied:"1/3",TotalTime:"1 ชั่วโมง 15 นาที",works:[{id:1,name:"ก้อง",tpyeslot:"",time:"15 นาที"},{id:2,name:"โจโจ้",tpyeslot:"",time:"15 นาที"},{id:3,name:"โดโด้",tpyeslot:"",time:"15 นาที"}]},
                     {title:"บทที่ 2: วิธีทำให้เท่ขึ้นเหมือนบูม",Studied:"1/3",TotalTime:"1 ชั่วโมง 15 นาที",works:[{id:1,name:"ก้อง",tpyeslot:"",time:"15 นาที"},{id:2,name:"โจโจ้",tpyeslot:"",time:"15 นาที"},{id:3,name:"โดโด้",tpyeslot:"",time:"15 นาที"}]},
@@ -9,7 +9,7 @@ function LearningClasses() {
             <div>
                 {courses.map(course => (
                     <div>
-                        <AnnulmentDropdown title={course.title} works={course.works} Studied={course.Studied} TotalTime={course.TotalTime}/>
+                        <AnnouncementDropdown title={course.title} works={course.works} Studied={course.Studied} TotalTime={course.TotalTime}/>
                     </div>
                 ))}
             </div>

beb3ec67d9474bae43c8db875b56016a75644966
peebaKU <baramee.254450@gmail.com>
2023-09-23 22:20:46 +0700

    fix: change directory

--- src/pages/students/LearningClasses.tsx
+++ src/pages/students/LearningClasses.tsx
@@ -1,4 +1,4 @@
-import AnnouncementDropdown from  "../component/AnnouncementDropdown"
+import AnnouncementDropdown from  "../../features/learns/components/AnnouncementDropdown"
 function LearningClasses() {
     const courses = [{title:"บทที่ 1: แนะนำผู้สอน ”บูม ” ",Studied:"1/3",TotalTime:"1 ชั่วโมง 15 นาที",works:[{id:1,name:"ก้อง",tpyeslot:"",time:"15 นาที"},{id:2,name:"โจโจ้",tpyeslot:"",time:"15 นาที"},{id:3,name:"โดโด้",tpyeslot:"",time:"15 นาที"}]},
                     {title:"บทที่ 2: วิธีทำให้เท่ขึ้นเหมือนบูม",Studied:"1/3",TotalTime:"1 ชั่วโมง 15 นาที",works:[{id:1,name:"ก้อง",tpyeslot:"",time:"15 นาที"},{id:2,name:"โจโจ้",tpyeslot:"",time:"15 นาที"},{id:3,name:"โดโด้",tpyeslot:"",time:"15 นาที"}]},

d708c107b3e83efa4e6b4b42ced016210da8843d
Nhuengzii <32nhueng@gmail.com>
2023-09-23 23:23:06 +0700

    fetchChapters in LearnCourse

--- src/pages/students/LearnCourse.tsx
+++ src/pages/students/LearnCourse.tsx
@@ -1,7 +1,27 @@
+import { useState } from "react"
 import { useParams } from "react-router-dom"
+import { fetchChapters } from "../../features/learns/services/progress"
 
 function LearnCourse() {
     const { courseID } = useParams()
+    const [fetching, setFetching] = useState(false)
+    const [chapters, setChapters] = useState([])
+
+    useState(() => {
+        async function listChapters() {
+            setFetching(true)
+            setFetching(false)
+            return await fetchChapters(courseID!)
+        }
+
+        listChapters().then((chapters) => {
+            console.log(JSON.stringify(chapters, null, 2));
+            setChapters(chapters)
+        })
+    }, [])
+
+    if (fetching) return (<div>Fetching...</div>)
+
     return (
         <div>LearnCourse {courseID}</div>
     )

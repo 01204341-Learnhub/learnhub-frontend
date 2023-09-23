f55fb1faa0e21fb2f19f5532fcb2d87a6e7c5858
Nhuengzii <32nhueng@gmail.com>
2023-09-23 22:42:57 +0700

    add fetchLessons service in learn feature

--- src/features/learns/services/progress.ts
+++ src/features/learns/services/progress.ts
@@ -12,4 +12,19 @@ async function fetchChapters(courseID: string) {
   return mockData;
 }
 
-export { fetchChapters };
+async function fetchLessons(courseID: string, chapterID: string) {
+  const mockDataGen = (n: number) => ({
+    lesson_num: n,
+    name: `Lesson ${n}`,
+    course_id: courseID,
+    chapter_id: chapterID,
+    lesson_type: "video",
+    src: "https://www.youtube.com/embed/2GJ8uNNJUH0",
+    description: "This is a lesson",
+  });
+
+  const mockData = Array.from({ length: 10 }, (_, i) => mockDataGen(i + 1));
+  return mockData;
+}
+
+export { fetchChapters, fetchLessons };

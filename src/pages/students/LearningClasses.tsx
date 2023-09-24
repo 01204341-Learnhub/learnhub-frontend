9f21038cacf82d24a15c9d134ce47bc2965a69e0
peebaKU <baramee.254450@gmail.com>
2023-09-24 12:47:37 +0700

    fix : change file name

--- src/pages/students/LearningClasses.tsx
+++ src/pages/students/LearningClasses.tsx
@@ -1,15 +1,16 @@
-import AnnouncementDropdown from  "../../features/learns/components/AnnouncementDropdown"
-function LearningClasses() {
-    const courses = [{title:"บทที่ 1: แนะนำผู้สอน ”บูม ” ",Studied:"1/3",TotalTime:"1 ชั่วโมง 15 นาที",works:[{id:1,name:"ก้อง",tpyeslot:"",time:"15 นาที"},{id:2,name:"โจโจ้",tpyeslot:"",time:"15 นาที"},{id:3,name:"โดโด้",tpyeslot:"",time:"15 นาที"}]},
-                    {title:"บทที่ 2: วิธีทำให้เท่ขึ้นเหมือนบูม",Studied:"1/3",TotalTime:"1 ชั่วโมง 15 นาที",works:[{id:1,name:"ก้อง",tpyeslot:"",time:"15 นาที"},{id:2,name:"โจโจ้",tpyeslot:"",time:"15 นาที"},{id:3,name:"โดโด้",tpyeslot:"",time:"15 นาที"}]},
-                    {title:"บทที่ 3: วิธีทำให้เก่งขึ้นเหมือนบูม",Studied:"1/3",TotalTime:"1 ชั่วโมง 15 นาที",works:[{id:1,name:"ก้อง",tpyeslot:"",time:"15 นาที"},{id:2,name:"โจโจ้",tpyeslot:"",time:"15 นาที"},{id:3,name:"โดโด้",tpyeslot:"",time:"15 นาที"}]}];
-
+import ChapterDropdown from  "../../features/learns/components/ChapterDropdown"
+export default function LearningClasses() {
+    const courses = [{chapterNum:1 ,name:'บทที่ 1: แนะนำผู้สอน ”บูม ” ',courseID:'A1',chapterLength:3600,lessonCount:5,lessoncompleted:3,lessons:[{name:"เริ่มจากต้องนั่งรถไฟไปเที่ยวก่อน",lessonType:"video", lessonID:"B1", finished:false, chapterID: "A1"
+},{name:"มารยาทในการนั่งรถไฟ",lessonType:"video", lessonID:"B1", finished:false, chapterID: "A1"
+}]},
+    {chapterNum:1 ,name:'บทที่ 2: วิธีทำให้เท่ขึ้นเหมือนบูม',courseID:'A1',chapterLength:3600,lessonCount:5,lessoncompleted:2,lessons:[{name:"มารยาทในการนั่งรถไฟ",lessonType:"video", lessonID:"B1", finished:true, chapterID: "A2"}]}]
     return (
         <>
             <div>
                 {courses.map(course => (
                     <div>
-                        <AnnouncementDropdown title={course.title} works={course.works} Studied={course.Studied} TotalTime={course.TotalTime}/>
+                        <ChapterDropdown chapterNum={course.chapterNum} name={course.name} courseID={course.courseID} chapterLength={course.chapterLength} lessonCount={course.lessonCount} 
+                        lessoncompleted={course.lessoncompleted} lessons={course.lessons}/>
                     </div>
                 ))}
             </div>
@@ -17,4 +18,3 @@ function LearningClasses() {
     )
 }
 
-export default LearningClasses

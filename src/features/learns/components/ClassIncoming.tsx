c6a22a1bc40a17d09093a64c0a259663911f2e06
Peerasu <fecownp@gmail.com>
2023-09-24 17:41:23 +0700

    add bg-white

--- src/features/learns/components/ClassIncoming.tsx
+++ src/features/learns/components/ClassIncoming.tsx
@@ -4,37 +4,37 @@ import { faClock } from "@fortawesome/free-solid-svg-icons";
 import { faChalkboardTeacher } from "@fortawesome/free-solid-svg-icons";
 
 interface ClassIncomingProps {
-    TitleName: string;
-    DayOfWeek: string;
-    Day: number;
-    Month: string;
-    Start: string;
-    End: string;
-    InstrName: string;
-    Color: string;
+    titleName: string;
+    dayOfWeek: string;
+    day: number;
+    month: string;
+    start: string;
+    end: string;
+    instrName: string;
+    color: string;
 }
 
 export default function ClassIncoming (props: ClassIncomingProps) { 
     return (
         <div className=" flex flex-cols-2 w-[460px] h-[150px] shadow-xl">
-            <div className={` bg-[${props.Color}] w-[10px]`}>
+            <div className={` bg-[${props.color}] w-[10px]`}>
             </div>
 
-            <div className=" flex flex-rows-3 relative">
+            <div className=" flex flex-rows-3 relative bg-white">
                 <div className=" space-y-[2%]">
-                    <p className=" font-bold text-3xl ml-[2%]">{props.TitleName}</p>
+                    <p className=" font-bold text-3xl ml-[2%]">{props.titleName}</p>
                     <hr className=" bg-slate-800 w-[450px] h-[2px]" />
                 </div>
                 <div className=" absolute left-3 top-16 flex space-x-3">
                     <FontAwesomeIcon icon={faCalendarDays} size="xl"></FontAwesomeIcon>
-                    <div>วัน{props.DayOfWeek}ที่ {props.Day} {props.Month}</div>
+                    <div>วัน{props.dayOfWeek}ที่ {props.day} {props.month}</div>
                     <div className=" font-extrabold text-[15px]"> | </div>
                     <FontAwesomeIcon icon={faClock} size="xl"></FontAwesomeIcon>
-                    <div>{props.Start} - {props.End}</div>
+                    <div>{props.start} - {props.end}</div>
                 </div>
                 <div className=" absolute left-2 top-28 flex space-x-3">
                     <FontAwesomeIcon icon={faChalkboardTeacher} size="xl"></FontAwesomeIcon>
-                    <div>{props.InstrName}</div>
+                    <div>{props.instrName}</div>
                 </div>
             </div>
         </div>

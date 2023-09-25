d400048e108d85c4817db806a4b97b60cb15733e
peebaKU <baramee.254450@gmail.com>
2023-09-25 11:14:58 +0700

    add : add pee's part

--- src/pages/students/LearningOverview.tsx
+++ src/pages/students/LearningOverview.tsx
@@ -1,32 +1,38 @@
+import Calendar from "../../features/learns/components/Calendar"
+import ClassHomeworks from "../../features/learns/components/ClassHomeworks"
+import ClassIncoming from "../../features/learns/components/ClassIncoming"
 import ClassTeachNow from "../../features/learns/components/ClassTeachNow"
 function LearningOverview() {
     return (
         <>
         <div className="flex">   
-            <div className="basis-3/4 bg-pink-300 ">
-                <div className="text-3xl font-bold m-9 mx-[10%]">การเรียนรู้ของฉัน</div>
-                <div className="text-xl font-bold m-7 mx-[10%]">คลาสเรียนที่กำลังสอนแล้ว</div>
-                <div className="mx-[10%] bg-gray-200 rounded-xl">
+            <div className="basis-3/4 ">
+                <div className="text-3xl font-bold m-9 mx-[10%] overflow-hidden">การเรียนรู้ของฉัน</div>
+                <div className="text-xl font-bold m-7 mx-[10%] overflow-hidden">คลาสเรียนที่กำลังสอนแล้ว</div>
+                <div className="mx-[10%] bg-gray-200 rounded-xl overflow-hidden">
                     <ClassTeachNow ThumbnailUrl={"https://miro.medium.com/v2/resize:fit:1400/0*mMBXJ9daWzStSCt5.jpg"} TitleName={"Principal Redstone Designs"} Minute={49.00} ></ClassTeachNow>
                 </div>
             </div>
-            <div className="basis-1/4 bg-green-500">
-            calculator
+            <div className="basis-1/4 bg-white m-10">
+            <Calendar></Calendar>
             </div>
         </div>
 
-        <div className="flex mx-[10%] bg-slate-400">
+        <div className="flex mx-[10%]">
             <div className="basis-2/5">
-                <div className="text-xl font-bold m-7 mx-[10%]">
+                <div className="text-2xl font-bold m-7 mx-[10%]">
                 คลาสเรียนที่ใกล้จะเริ่มสอน
                 </div>
-                <div>
-                    
+                <div className="m-5">
+                    <ClassIncoming titleName={"Principal Redstone Designs"} dayOfWeek={"ศุกร์"} day={19} month={"กรกฏาคม"} start={"10.00"} end={"12.00"} instrName={"by Instructor Name1"} color={"#B1E8ED"}></ClassIncoming>
                 </div>
             </div>
-            <div className="basis-3/5">
-                <div className="text-xl font-bold m-7 mx-[10%]">
-                การบ้านในคลาสที่ต้องส่งเร็วๆนี้
+            <div className="basis-3/5 mx-5">
+                <div className="text-2xl font-bold m-7 mx-[10%]">
+                    การบ้านในคลาสที่ต้องส่งเร็วๆนี้
+                </div>
+                <div className="m-5">
+                    <ClassHomeworks homeworks={[]}></ClassHomeworks>
                 </div>
             </div>
         </div>

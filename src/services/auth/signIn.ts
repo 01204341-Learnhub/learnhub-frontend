3a8d02f4e9a424cbf956927fff25814488af6d74
Nhuengzii <32nhueng@gmail.com>
2023-09-30 23:37:28 +0700

    add two more function in service auth
    createStudentWithEmail and signInwithEmail

--- src/services/auth/signIn.ts
+++ src/services/auth/signIn.ts
@@ -1,6 +1,13 @@
-import { getAuth, signInAnonymously } from "firebase/auth";
+import axios from "axios";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInAnonymously,
+  signInWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
 import { app } from "../../firebase/firebase";
-import { LearnhubUser } from "../../types/user";
+import { LearnhubUser, LearnhubUserResponse } from "../../types/user";
 
 const auth = getAuth(app);
 
@@ -23,4 +30,72 @@ async function signIn() {
   }
 }
 
-export { signIn };
+async function createStudentWithEmail(
+  email: string,
+  password: string,
+  username: string,
+  fullName: string
+) {
+  try {
+    const userCredentail = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    const uid = userCredentail.user.uid;
+    const url: string = "http://localhost:8000/users/students";
+    const body = {
+      uid: uid,
+      username: username,
+      fullname: fullName,
+      email: email,
+    };
+    const res = await axios.post<{ student_id: string }>(url, body);
+    const studentID = res.data.student_id;
+
+    // change fullname of firebase
+    const user = userCredentail.user;
+    await updateProfile(user, { displayName: studentID });
+    const learnhubUser: LearnhubUser = {
+      userType: "student",
+      userID: uid,
+      username: username,
+      email: email,
+      profilePicture:
+        "https://images.theconversation.com/files/102848/original/image-20151123-18264-j336wc.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=926&fit=clip",
+    };
+    return learnhubUser;
+  } catch (error) {
+    console.log(`error when create student with email`, error);
+  }
+}
+
+async function signInWithEmail(
+  email: string,
+  password: string,
+  userType: "student" | "teacher"
+) {
+  try {
+    const userCredential = await signInWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    const uid = userCredential.user.uid;
+    const learnhubUID = userCredential.user.displayName;
+    const url: string = `http://localhost:8000/users/${userType}s/${learnhubUID}`;
+    const data = (await axios.get<LearnhubUserResponse>(url)).data;
+    const learnhubUser: LearnhubUser = {
+      userType: userType,
+      userID: uid,
+      username: data.username,
+      email: data.email,
+      profilePicture: data.profile_pic,
+    };
+    return learnhubUser;
+  } catch (error) {
+    console.log(`error when sign in with email`, error);
+  }
+}
+
+export { createStudentWithEmail, signIn, signInWithEmail };

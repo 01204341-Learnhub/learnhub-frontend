57b0e9d18ae5247b986f14a2aaf9cad97aff0f26
mahasachan <mahasachan.sa@ku.th>
2023-09-30 18:04:53 +0700

    add ResponseGetCourseId Type

--- src/features/stores/types/response.ts
+++ src/features/stores/types/response.ts
@@ -1,19 +1,54 @@
 
 type Tag = {
-    tagID: string
+    tagId: string
     tagName: string
 }
 
-export type ResponseGetCourse = {
-    courseID: string
+type ResponseData = {
+    course_id: string
     name: string
-    coursePic: string
+    course_pic: string
     teacher: {
-        teacherID: string
-        teacherName: string
+        teacher_id: string
+        teacher_name: string
     }
-    tags: Tag[]
+    tags: {
+        tag_id: string
+        tag_name : string
+    }[]
     rating: number
-    reviewCount: number
+    review_count: number
     price: number
+}
+
+
+export type ResponseGetCourseId = {
+    course_id : string
+    name : string
+    course_pic : string
+    tags : {
+        tag_id : string
+        tag_name : string
+    }[]
+    description : string
+    course_objective : string
+    course_requirement : string
+    difficulty_level : string
+    rating : number
+    review_count : number
+    student_count : number
+    teacher : {
+        teacher_id : string
+        teacher_name : string
+    },
+    price: number
+    total_video_length : number
+    chapter_count : number
+    quiz_count : number
+    file_count : number
+    video_count : number
+}
+
+export type ResponseGetCourses = {
+    courses : ResponseData[]
 }

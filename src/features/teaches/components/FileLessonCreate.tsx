408619339c15417846155992391fa487785b9fa2
gitrinz99 <mightyrinz@gmail.com>
2023-09-30 17:34:47 +0700

    FileLessonCreate change appearance

--- src/features/teaches/components/FileLessonCreate.tsx
+++ src/features/teaches/components/FileLessonCreate.tsx
@@ -1,4 +1,4 @@
-import { faX } from "@fortawesome/free-solid-svg-icons"
+import { faX, faUpload } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { useState } from "react"
 import { CourseLesson } from "../types/course"
@@ -43,10 +43,22 @@ function FileLessonCreate({ chapterName, chapterNumber, lessonNumber, onCancel, 
 
     return (
         <div>
-            <h1>สร้างคอร์สเรียน</h1>
-            <h1>บทที่ {chapterNumber}: </h1>
-            <h2>{chapterName} / ไฟล์</h2>
-            <div className="bg-white">
+            <div className="ml-[70px] mt-[20px] flex items-center">
+                <h1 className="text-black text-[24px] font-bold">บทที่ {chapterNumber} : </h1>
+                <h2 className=" text-[#808080] text-[18px] font-semibold ml-4">{chapterName} / ไฟล์</h2>
+            </div>
+            <div className="ml-[70px] mr-[100px] mt-[30px] bg-white drop-shadow-xl">
+                <div className="  flex grow items-center pt-2 pb-4">
+                    <h1 className="my-auto mx-[40px] font-semibold text-[18px]">หัวข้อ</h1>
+                    <input type="text" className="mr-[50px] min-w-0 grow input input-bordered" value={lessonName} onChange={onLessonNameChange} />
+                </div>
+                <div className="  flex grow items-center pt-2 pb-4">
+                    <h1 className="my-auto mx-[40px] font-semibold text-[18px]">คำอธิบาย</h1>
+                    <input type="text" className="mr-[50px] min-w-0  grow input input-bordered" value={lessonDescription} onChange={onLessonDescriptionChange} />
+                </div>
+            </div>
+
+            {/* <div className="bg-white">
                 <div className="flex">
                     <h1>หัวข้อ</h1>
                     <input type="text" className="input" value={lessonName} onChange={onLessonNameChange} />
@@ -55,24 +67,29 @@ function FileLessonCreate({ chapterName, chapterNumber, lessonNumber, onCancel, 
                     <h1>คำอธิบาย</h1>
                     <input type="text" className="input" value={lessonDescription} onChange={onLessonDescriptionChange} />
                 </div>
-            </div>
-            <div>
+            </div> */}
+            <div className="ml-[70px] mr-[100px] mt-[30px] bg-white drop-shadow-xl">
                 <ol>
                     {files.map((file, index) => (
                         <li key={index}>
-                            <div className="flex">
-                                <h1>{file.name}</h1>
-                                <FontAwesomeIcon icon={faX} onClick={() => onRemoveFile(index)} />
+                            <div className=" ml-[40px] mr-[100px] mt-[20px] flex bg-white drop-shadow-xl">
+                                <h1 className=" ml-[40px] mr-[50px] my-[20px] grow font-semibold text-[#808080] text-[18px]">{file.name}</h1>
+                                <FontAwesomeIcon icon={faX} color="#606060" className=" mx-[40px] my-[20px]" onClick={() => onRemoveFile(index)} />
                             </div>
                         </li>
+                        
                     ))}
                 </ol>
                 <input type="file" id="lessonFileSelector" style={{ display: "none" }} onChange={handleFileChange} />
-                <button className="btn" onClick={() => { document.getElementById("lessonFileSelector").click() }}>เพิ่มไฟล์</button>
+                <button className="btn mx-[40px] my-[20px] text-[#808080]" onClick={() => { document.getElementById("lessonFileSelector").click() }}>
+                    <FontAwesomeIcon icon={faUpload} color="#606060" size="xl" className=" border-[#e0e0e0] border-2 p-2 rounded-full" />
+                    เพิ่มไฟล์
+                </button>
+                
             </div>
-            <div className="flex justify-end w-full">
+            <div className="flex mt-[40px] justify-end mr-[100px] w-full">
                 <button className="btn bg-black text-white" onClick={handleSubmit}>บันทึก</button>
-                <button className="btn ml-7" onClick={onCancel}>ยกเลิก</button>
+                <button className="btn ml-7 mr-[100px]" onClick={onCancel}>ยกเลิก</button>
             </div>
         </div>
     )

287c953f6b866d75270ed51f5a0d3fe09065c7d8
mahasachan <mahasachan.sa@ku.th>
2023-10-01 17:53:07 +0700

    improve UI

--- src/components/StudentNavBar.tsx
+++ src/components/StudentNavBar.tsx
@@ -4,26 +4,26 @@ import { NavLink } from "react-router-dom"
 
 function StudentNavBar() {
     return (
-        <div className="">
-            <h1 className="px-2 ml-2 mt-3 bg-teal-400 rounded-xl inline-block">ผู้เรียน</h1>
-            <NavLink to="/learn/overview" replace={true} className={({ isActive }) => `flex my-3 hover:bg-gray-400 ${isActive ? "bg-gray-400" : ""}`}>
-                <FontAwesomeIcon icon={faBookOpen} />
+        <div className="flex flex-col items-start px-4">
+            <h1 className="px-4 py-0.5 mb-6 ml-2 mt-3 bg-teal-400 rounded-xl text-base font-semibold inline-block">ผู้เรียน</h1>
+            <NavLink to="/learn/overview" replace={true} className={({ isActive }) => `flex items-center w-full my-2 py-2 hover:bg-[#d9d9d9] ${isActive ? "bg-[#d9d9d9]" : ""}`}>
+                <FontAwesomeIcon icon={faBookOpen} size="xl" className="pl-6 pr-3" />
                 <h1 className="ml-2 font-bold text-2xl">ภาพรวม</h1>
             </NavLink>
-            <NavLink to="/learn/courses" replace={true} className={({ isActive }) => `flex my-3 hover:bg-gray-400 ${isActive ? "bg-gray-400" : ""}`}>
-                <FontAwesomeIcon icon={faBookOpen} />
+            <NavLink to="/learn/courses" replace={true} className={({ isActive }) => `flex items-center w-full my-2 py-2 hover:bg-[#d9d9d9] ${isActive ? "bg-[#d9d9d9]" : ""}`}>
+                <FontAwesomeIcon icon={faBookOpen} size="xl" className="pl-6 pr-3"/>
                 <h1 className="ml-2 font-bold text-2xl">คอร์สเรียนของฉัน</h1>
             </NavLink>
-            <NavLink to="/learn/classes" replace={true} className={({ isActive }) => `flex my-3 hover:bg-gray-400 ${isActive ? "bg-gray-400" : ""}`}>
-                <FontAwesomeIcon icon={faScaleBalanced} />
+            <NavLink to="/learn/classes" replace={true} className={({ isActive }) => `flex items-center  w-full my-2 py-2 hover:bg-[#d9d9d9] ${isActive ? "bg-[#d9d9d9]" : ""}`}>
+                <FontAwesomeIcon icon={faScaleBalanced} size="xl" className="pl-6 pr-3"/>
                 <h1 className="ml-2 font-bold text-2xl">คลาสเรียนของฉัน</h1>
             </NavLink>
-            <NavLink to="/learn/homework" replace={true} className={({ isActive }) => `flex my-3 hover:bg-gray-400 ${isActive ? "bg-gray-400" : ""}`}>
-                <FontAwesomeIcon icon={faClipboardList} />
+            <NavLink to="/learn/homework" replace={true} className={({ isActive }) => `flex items-center w-full my-2 py-2 hover:bg-[#d9d9d9] ${isActive ? "bg-[#d9d9d9]" : ""}`}>
+                <FontAwesomeIcon icon={faClipboardList} size="xl" className="pl-6 pr-3"/>
                 <h1 className="ml-2 font-bold text-2xl">งานที่ต้องทำ</h1>
             </NavLink>
-            <NavLink to="/learn/schedule" replace={true} className={({ isActive }) => `flex my-3 hover:bg-gray-400 ${isActive ? "bg-gray-400" : ""}`}>
-                <FontAwesomeIcon icon={faCalendar} />
+            <NavLink to="/learn/schedule" replace={true} className={({ isActive }) => `flex items-center w-full my-2 py-2 hover:bg-[#d9d9d9] ${isActive ? "bg-[#d9d9d9]" : ""}`}>
+                <FontAwesomeIcon icon={faCalendar} size="xl" className="pl-6 pr-3" />
                 <h1 className="ml-2 font-bold text-2xl">ตารางเรียน</h1>
             </NavLink>
         </div>

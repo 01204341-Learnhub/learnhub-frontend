04c9aa771db4044d6e268e11bdb3c072e8d28223
Narawit Sariddeechaikul <narawitpun@gmail.com>
2023-10-01 17:53:53 +0700

    add file skeletons for create class

---
+++ src/features/teaches/components/ClassCardPreview.tsx
@@ -0,0 +1,14 @@
+interface ClassCardPreviewProps {
+  classThumbnailUrl: string;
+  className: string;
+  instructorName: string;
+  price: number;
+  category: string;
+  level: string;
+}
+
+function ClassCardPreview(props: ClassCardPreviewProps) {
+  return <div></div>;
+}
+
+export default ClassCardPreview;

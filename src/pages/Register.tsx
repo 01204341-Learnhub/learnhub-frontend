f4e57bbcdc65750265ff0d7dfd2262929922826e
Nhuengzii <32nhueng@gmail.com>
2023-10-02 12:19:50 +0700

    fix bug wrong pathname

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -14,7 +14,7 @@ function Register() {
     }
 
     function handleClickRegister() {
-        navigate("/", { replace: true })
+        navigate("/login", { replace: true })
     }
 
     function handleClickStudent() {

9dd8770d1c0e28d84d0289ee8405d4cfd22e6226
mahasachan <mahasachan.sa@ku.th>
2023-10-04 00:26:53 +0700

    add state isLoading & fetchOnce and func to update

--- src/slices/basketSlice.ts
+++ src/slices/basketSlice.ts
@@ -1,43 +1,16 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { Basket, BasketItem } from "../features/stores/types/basket";
 
-const mockBasket: Basket = {
-  items: [
-    {
-      thumbnailURL:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbx7eE-vThApajEf_UUzk6v18mYG3BxAvEvw&usqp=CAU",
-      programID: "1",
-      name: "Item 1 bra bra bra bra bra",
-      price: 100,
-      description: "This is item 1",
-      itemID: "1",
-      teacherName: "Bara MEEHUMYAI",
-      rating: 4.5,
-      voter: 32465,
-      totalTime: 159,
-      lvl: "พื้นฐาน"
-    },
-    {
-      thumbnailURL:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbx7eE-vThApajEf_UUzk6v18mYG3BxAvEvw&usqp=CAU",
-      programID: "2",
-      name: "Item 2",
-      price: 150,
-      description: "This is item 2",
-      itemID: "2",
-      teacherName: "Bara MEEHUMYAI",
-      rating: 4.5,
-      voter: 32465,
-      totalTime: 159,
-      lvl: "พื้นฐาน"
-    },
-  ],
-};
 
 export const basketSlice = createSlice({
   name: "basket",
   initialState: {
-    basket: mockBasket as Basket,
+    basket: {
+      items: []
+    } as Basket,
+    isFetchOnce : false,
+    iSLoading : false
+
   },
   reducers: {
     addItem: (state, action) => {
@@ -54,7 +27,15 @@ export const basketSlice = createSlice({
     clearItem: (state) => {
       state.basket.items = [];
     },
+
+    setStatusFetchOnce : (state, action) => {
+      state.isFetchOnce = action.payload as boolean
+    },
+
+    setStatusIsLoading : (state, action) => {
+      state.iSLoading = action.payload as boolean
+    }
   },
 });
 export default basketSlice.reducer;
-export const { addItem, removeItem, clearItem } = basketSlice.actions;
+export const { addItem, removeItem, clearItem, setStatusFetchOnce, setStatusIsLoading } = basketSlice.actions;

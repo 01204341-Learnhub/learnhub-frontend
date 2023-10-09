f7aeefcc28706dfff0240f52217f561e33563ff6
mahasachan <mahasachan.sa@ku.th>
2023-10-09 21:25:10 +0700

    add FullName & Email

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -8,6 +8,10 @@ function Profile() {
   const dispatch = useDispatch();
   const [selectProfile, setSelectProfile] = useState<boolean>(true);
   const [selectSetting, setSelectSetting] = useState<boolean>(false);
+  const [firstName, setFirstName] = useState<string>("");
+  const [lastName, setLastName] = useState<string>("");
+  const [isEdit, setIsEdit] = useState<boolean>(false);
+  const [isChangePassword, setIsChangePassword] = useState<boolean>(false);
 
   const classNameSelectProfile = selectProfile
     ? "border-b-[10px] border-black ml-2 py-2 px-3 border-b-[12px] z-10"
@@ -25,6 +29,32 @@ function Profile() {
     setSelectProfile(false);
     setSelectSetting(true);
   };
+
+  const handleClickEditProfile = () => {
+    setIsEdit(!isEdit);
+  }
+
+  const ChangeFirstName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFirstName(e.target.value);
+  }
+
+  const ChangeLastName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setLastName(e.target.value);
+  }
+
+  
+
+  const classTextIsExist = " h-16 text-base font-medium text-[#505050]"; 
+  const classTextNotExist = "h-16 text-base font-medium text-[#808080]";
+  const classText = (value : string) => {
+    if (value.length > 0) {
+      return classTextIsExist;
+    } else {
+      return classTextNotExist;
+    }
+  }
+
+
   if (isFetching) {
     console.log("isFetching", isFetching);
 
@@ -34,6 +64,139 @@ function Profile() {
       </div>
     );
   }
+
+  const ButtonEditProfile = () => {
+    return (
+      <button
+        onClick={handleClickEditProfile}
+        type="button"
+        className="bg-gray-100 px-4 py-2 mx-36 self-end rounded-md font-semibold mt-4"
+      >
+        { isEdit ? "บันทึก" : "แก้ไข" }
+      </button>
+    );
+  }
+
+
+  const renderProfile = () => {
+    return (
+      <div className="h-full w-[60%] px-20 pt-8 mt-8">
+          <div className="flex justify-between">
+            <div className="flex flex-col w-full mr-24">
+              <h1 className="text-2xl font-semibold">{ user.username }</h1>
+              <p className="text-base py-2">
+                ทุกคนจะเห็นช์่อนี้บน โปรไฟล์ของคุณและสามารถเปลี่ยนได้ทุกเมื่อ
+              </p>
+              <div className="flex flex-col items-start">
+                <label htmlFor="FirstName" className="font-semibold text-lg py-2 mt-4">ชื่อจริง</label>
+
+                { isEdit ? (
+                  <input
+                    onChange={ChangeFirstName}
+                    placeholder="ชื่อจริง"
+                    className="border-2 border-gray-300 outline-none h-12 w-4/5 px-4 mb-4" 
+                    type="text" 
+                    name="FirstName" 
+                    id="FirstName" />
+                ) :  <span className={classText(firstName)}>{ firstName ? firstName : "คุณยังไม่ตั้งชื่อของคุณ"   }</span> }
+
+                <label htmlFor="LastName" className="font-semibold text-lg py-2">นามสกุล</label>
+                 { isEdit ? (
+                  <input
+                    onChange={ChangeLastName}
+                    placeholder="นามสกุล"
+                    className="border-2 border-gray-300 outline-none h-12 w-4/5 px-4 mb-4" 
+                    type="text" 
+                    name="LastName" 
+                    id="LastName" />
+                 ) : <span className={classText(lastName)}>{ lastName ? lastName : "คุณยังไม่ตั้งนามสกุลของคุณ" }</span> }
+
+            
+                <label htmlFor="Occupation" className="font-semibold text-lg py-2">อาชีพ</label>
+                
+            
+
+                { ButtonEditProfile() }
+              </div>
+            </div>
+
+
+            <div className="flex flex-col avatar relative">
+              <div className="w-40 h-40 rounded-full">
+                <img src={user.profilePicture} />
+              </div>
+              <input
+                type="file"
+                id="editProfilePicture"
+                style={{ display: "none" }}
+                value={[]}
+                onChange={(e) => {
+                  dispatch(changeProfilePicture(URL.createObjectURL(e.target.files![0])));
+                }}
+              />
+              <button className="bg-gray-100 font-semibold w-20 absolute top-40 left-24"
+                onClick={() => { document.getElementById("editProfilePicture").click() }}>
+                แก้ไข
+              </button>
+            </div>
+          </div>
+        </div>
+    )
+  }
+
+
+  const handleChangePassword = () => {
+    setIsChangePassword(!isChangePassword);
+  }
+
+  const modalChangePassword = () => {
+    return (
+      <div className="fixed inset-0 z-50 flex items-center justify-center">
+      <div className="absolute inset-0 bg-gray-900 opacity-50"></div>
+      <div className="bg-white rounded-lg p-6">
+        <div className="flex justify-end">
+          <button onClick={handleChangePassword}>Close</button>
+        </div>
+        <p> eiei {isChangePassword}</p>
+      </div>
+    </div>
+    )
+  }
+
+  const renderSetting = () => {
+    return (
+      <div className="w-4/5 h-full">
+        <div className="flex flex-col items-start pt-20">
+          <label 
+            className="font-semibold text-xl py-4"
+            htmlFor="Email"
+            >Email</label>
+          <p className="font-semibold text-[#808080] border-2 w-96 bg-[#f5f5f580] px-4 py-4">{user.email}</p>
+          {/* <input
+            className="border-2 border-[#808080] bg-[#f5f5f5] outline-none h-12 w-2/5 px-4 mb-4" 
+            type="text" name="Email" id="Email" /> */}
+
+          <label
+            className="font-semibold text-xl py-4" 
+            htmlFor="Password"
+            >Password</label>
+          <p className="font-semibold tracking-[6px] text-[#808080] bg-[#f5f5f580] p-4 border-2 w-96 ">**********</p>
+          {/* <input
+            className="border-2 border-[#808080] bg-[#f5f5f5] outline-none h-12 w-2/5 px-4 mb-4" 
+            type="text" name="Password" id="Password" /> */}
+          <button
+            onClick={handleChangePassword}
+            type="button"
+            className=" text-[#068FFF] text-xl rounded-md font-semibold mt-4"
+          >
+            เปลี่ยนรหัสผ่าน
+          </button>
+          { isChangePassword ? modalChangePassword() : null }
+        </div>
+      </div>
+    )
+  }
+
 
   return (
     <div className="flex flex-col items-center h-full relative">
@@ -54,35 +217,10 @@ function Profile() {
         </button>
       </div>
       <div className="h-3 w-full bg-gray-200 absolute top-14 z-0"></div>
-      <div className="h-full w-[60%] px-20 pt-8 mt-8">
-        <div className="flex justify-between">
-          <div>
-            <h1 className="text-2xl font-semibold">ชื่อของคุณ</h1>
-            <p className="text-base py-2">
-              ทุกคนจะเห็นช์่อนี้บน โปรไฟล์ของคุณและสามารถเปลี่ยนได้ทุกเมื่อ
-            </p>
-            <h2 className="font-bold">{user.username}</h2>
-          </div>
-          <div className="flex flex-col avatar relative">
-            <div className="w-40 h-40 rounded-full">
-              <img src={user.profilePicture} />
-            </div>
-            <input
-              type="file"
-              id="editProfilePicture"
-              style={{ display: "none" }}
-              value={[]}
-              onChange={(e) => {
-                dispatch(changeProfilePicture(URL.createObjectURL(e.target.files![0])));
-              }}
-            />
-            <button className="bg-gray-100 font-semibold w-20 absolute bottom-[-15px] left-24"
-              onClick={() => { document.getElementById("editProfilePicture").click() }}>
-              แก้ไข
-            </button>
-          </div>
-        </div>
-      </div>
+      
+      { selectProfile ? (
+        renderProfile()
+      ) : renderSetting() }
     </div>
   );
 }

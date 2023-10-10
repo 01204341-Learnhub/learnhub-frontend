a781b0fd9ef9e3ed763458552010a1cc8d61f681
gitrinz99 <mightyrinz@gmail.com>
2023-10-10 22:06:28 +0700

    MainBar for teacher

--- src/components/MainBar.tsx
+++ src/components/MainBar.tsx
@@ -54,63 +54,70 @@ function MainBar() {
                 <img className=' w-2/5' src={booklogo} alt="booklogo" />
             </div>
             <div className='flex flex-row items-center w-2/12' onClick={() => { navigate("/home") }}>
-                <img className='' src={namelogo} alt="namelogo" />
+                <img src={namelogo} alt="namelogo" />
             </div>
-            <div className='bg-gray-300 flex rounded-full w-2/4 py-2 px-2'>
-                <FontAwesomeIcon icon={faMagnifyingGlass} color='black' size='xl' className='mr-4 ml-4' />
-                <input
-                    type="search"
-                    className=" bg-transparent border-none outline-none w-full text-[18px] font-semibold "
-                />
-            </div>
-            <div className='w-2/12 flex justify-evenly my-2'>
-                <Link to={"/learn/overview"}>
-                    <FontAwesomeIcon icon={faBook} size='xl' />
-                </Link>
-                <button onClick={() => toggleDropdown('mypindropdown')}>
-                    <FontAwesomeIcon icon={faHeart} size='xl' color={openDropdown === 'mypindropdown' ? '#FF2171' : 'none'} />
+            { user && user.userType ==='teacher' ? <div className='w-2/4 py-2 px-2'/> : ( 
+                <div className='bg-gray-300 flex rounded-full w-2/4 py-2 px-2'>
+                    <FontAwesomeIcon icon={faMagnifyingGlass} color='black' size='xl' className='mr-4 ml-4' />
+                    <input
+                        type="search"
+                        className=" bg-transparent border-none outline-none w-full text-[18px] font-semibold "
+                    />
+                </div>
+            )}
+            <div className='w-2/12 grid grid-cols-4 my-2'>
+                { user && user.userType==='teacher' ? null : (
+                    <div className=' col-span-3 grid grid-cols-3'>
+                        <button>
+                            <Link to={"/learn/overview"}>
+                                <FontAwesomeIcon icon={faBook} size='xl' />
+                            </Link>
+                        </button>
+                        
+                        <button onClick={() => toggleDropdown('mypindropdown')}>
+                            <FontAwesomeIcon icon={faHeart} size='xl' color={openDropdown === 'mypindropdown' ? '#FF2171' : 'none'} />
 
-                    {/* Mypin dropdown menu */}
-                    <div
-                        style={{ display: openDropdown === 'mypindropdown' ? 'block' : 'none', top: '90%', right: '17%' }}
-                        className=" absolute w-[320px] h-[550px] bg-white border border-gray-300 rounded-lg shadow divide-y divide-gray-100"
-                    >
-                        <div className="px-8 py-3">
+                            {/* Mypin dropdown menu */}
+                            <div
+                                style={{ display: openDropdown === 'mypindropdown' ? 'block' : 'none', top: '90%', right: '17%' }}
+                                className=" absolute w-[320px] h-[550px] bg-white border border-gray-300 rounded-lg shadow divide-y divide-gray-100"
+                            >
+                                <div className="px-8 py-3">
 
-                        </div>
-                    </div>
-                    {/* Mypin dropdown menu */}
-
-                </button>
-                <button onClick={handleClickBasket}>
-                    <FontAwesomeIcon icon={faCartShopping} size='xl' color={openDropdown === 'mycartdropdown' ? 'red' : 'none'} />
-
-                    {/* Mycart dropdown menu */}
-                    <div className='flex flex-col  absolute w-[320px] bg-white border border-gray-300 rounded-lg shadow divide-y divide-gray-100'
-                        style={{ display: openDropdown === 'mycartdropdown' ? 'block' : 'none', top: '90%', right: '13%' }}
-                    >
-                        <div
-                            className='overflow-y-auto max-h-[450px] min-h-0'
-                            style={{ display: openDropdown === 'mycartdropdown' ? 'block' : 'none', top: '90%', right: '13%' }}
-                            
-                        >
-                            <div className="px-8 py-3">
-                                {basketItems.map((item) => (
-                                    <div key={item.itemID}>
-                                        <BasketItemSlot item={item} />
-                                    </div>
-                                ))}
+                                </div>
                             </div>
-                        </div>
-                        <h1 className=' font-bold text-[20px]'>{basketItems.reduce((acc, item) => acc + item.price, 0)} บาท</h1>
-                        <div className='bg-[#d9d9d9] px-[40px] py-3 m-2'>
-                            <Link to={{ pathname: "/baskets" }} className=' font-semibold text-[20px]'>ไปยังรถเข็น</Link>
-                        </div>
-                    </div>
-                    {/* Mycart dropdown menu */}
+                        </button>
 
-                </button>
-                <button onClick={() => toggleDropdown('mynotidropdown')}>
+                        <button onClick={handleClickBasket}>
+                            <FontAwesomeIcon icon={faCartShopping} size='xl' color={openDropdown === 'mycartdropdown' ? 'red' : 'none'} />
+
+                            {/* Mycart dropdown menu */}
+                            <div className='flex flex-col  absolute w-[320px] bg-white border border-gray-300 rounded-lg shadow divide-y divide-gray-100'
+                                style={{ display: openDropdown === 'mycartdropdown' ? 'block' : 'none', top: '90%', right: '13%' }}
+                            >
+                                <div
+                                    className='overflow-y-auto max-h-[450px] min-h-0'
+                                    style={{ display: openDropdown === 'mycartdropdown' ? 'block' : 'none', top: '90%', right: '13%' }}
+                                    
+                                >
+                                    <div className="px-8 py-3">
+                                        {basketItems.map((item) => (
+                                            <div key={item.itemID}>
+                                                <BasketItemSlot item={item} />
+                                            </div>
+                                        ))}
+                                    </div>
+                                </div>
+                                <h1 className=' font-bold text-[20px]'>{basketItems.reduce((acc, item) => acc + item.price, 0)} บาท</h1>
+                                <div className='bg-[#d9d9d9] px-[40px] py-3 m-2'>
+                                    <Link to={{ pathname: "/baskets" }} className=' font-semibold text-[20px]'>ไปยังรถเข็น</Link>
+                                </div>
+                            </div>
+                        </button>
+                    </div>
+                )}
+
+                <button className=' col-start-4' onClick={() => toggleDropdown('mynotidropdown')}>
                     <FontAwesomeIcon icon={faBell} size='xl' color={openDropdown === 'mynotidropdown' ? '#F1C93B' : 'none'} />
 
                     {/* MyNotification dropdown menu */}
@@ -122,8 +129,6 @@ function MainBar() {
 
                         </div>
                     </div>
-                    {/* MyNotification dropdown menu */}
-
                 </button>
             </div>
             {
@@ -143,7 +148,10 @@ function MainBar() {
                                 >
                                     <div className="px-8 py-3">
                                         <img src={user.profilePicture} alt="profile" className=' h-[50px] w-[50px] rounded-full' />
-                                        <span className="block text-[18px] font-bold text-black text-left">{user.username}</span>
+                                        <div className='flex justify-between items-center'>
+                                            <span className="block text-[18px] font-bold text-black text-left">{user.username}</span>
+                                            <div className=' bg-[#A1CCD1] rounded-full px-3 py-1 font-bold text-[13px]'>{user.userType}</div>
+                                        </div>
                                         <span className="block text-[16px]  text-gray-500 truncate text-left ">{user.email}</span>
                                     </div>
                                     <ul className="py-2" aria-labelledby="user-menu-button">

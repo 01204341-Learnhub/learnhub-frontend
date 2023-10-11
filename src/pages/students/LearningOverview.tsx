1e0146d8eadb56784d2e3f0d3bdc5a668cf2c981
peebaKU <baramee.254450@gmail.com>
2023-10-11 21:09:09 +0700

    fetch data from backend

--- src/pages/students/LearningOverview.tsx
+++ src/pages/students/LearningOverview.tsx
@@ -6,24 +6,21 @@ import { useStudentDashboard } from "../../features/learns/hooks/useStudentDashb
 import { useUser } from "../../hooks/useUser"
 
 
-const DataHomework =[{
-    titleName:"What is Redstone1" ,homeworkName:"Principal Redstone Designs" ,deadlineDayOfWeek:"" ,deadlineTime:"พรุ่งนี้ 9.00" ,status:"ยังไม่ได้ส่ง" ,color:"red"
-},{
-    titleName:"What is Redstone2" ,homeworkName:"Principal Redstone Designs" ,deadlineDayOfWeek:"" ,deadlineTime:"พรุ่งนี้ 12.00" ,status:"ยังไม่ได้ส่ง" ,color:"red"
-},{
-    titleName:"What is Redstone3" ,homeworkName:"Principal Redstone Designs" ,deadlineDayOfWeek:"" ,deadlineTime:"พรุ่งนี้ 15.00" ,status:"ยังไม่ได้ส่ง" ,color:"red"
-},{
-    titleName:"What is Redstone4" ,homeworkName:"Principal Redstone Designs" ,deadlineDayOfWeek:"" ,deadlineTime:"พรุ่งนี้ 20.00" ,status:"ยังไม่ได้ส่ง" ,color:"red"
-},{
-    titleName:"What is Redstone5" ,homeworkName:"Principal Redstone Designs" ,deadlineDayOfWeek:"" ,deadlineTime:"พรุ่งนี้ 23.59" ,status:"ยังไม่ได้ส่ง" ,color:"red"
-}]
 
 
 
 function LearningOverview() {
     const {user} = useUser()
     const {dashboard,isFetching}=useStudentDashboard(user.userID)
-    
+    function convertTimestampToDate(timestamp: number): Date {
+        return new Date(timestamp * 1000);
+    }
+    function formatTimeFromTimestamp(timestamp: number): string {
+        const date = new Date(timestamp);
+        const hours = date.getHours().toString().padStart(2, '0');
+        const minutes = date.getMinutes().toString().padStart(2, '0');
+        return `${hours}:${minutes}`;
+      }
     if(isFetching) return(<div>loading......</div>)
     return (
         <div className="overflow-hidden flex justify-center">
@@ -36,7 +33,7 @@ function LearningOverview() {
                             <ClassTeachNow 
                             ThumbnailUrl={"https://image.makewebeasy.net/makeweb/m_1920x0/FZoJyLend/Home/zone9_pic3.png"} 
                             TitleName={"Principal Redstone Designs"} Minute={49.00} />
-                        
+                            
                     </div>
                     <div className="flex items-center justify-center bg-white mb-4">
                         <Calendar/>
@@ -47,50 +44,59 @@ function LearningOverview() {
                     <div className="flex flex-col bg-[#f5f5f580]">
                         <div className="text-2xl mt-10 pb-4 font-bold">คลาสเรียนที่ใกล้จะเริ่มสอน</div>
                         <div className="mr-52">
-                            <ClassIncoming 
-                                    titleName={"Principal Redstone Designs"} 
-                                    dayOfWeek={"ศุกร์"} 
-                                    day={19} 
-                                    month={"กรกฏาคม"} 
-                                    start={"10.00"} 
-                                    end={"12.00"} 
-                                    instrName={"by Instructor Name1"} 
+                        {dashboard.classes.map(({ classInfo, teacher, schedules }) => (
+                            <li key={classInfo.classID}  className={`flex justify-center mt-2`}>
+                                <ClassIncoming
+                                    titleName={classInfo.className} 
+                                    date={convertTimestampToDate(schedules[0].start).toDateString()} 
+                                    start={`${formatTimeFromTimestamp(schedules[0].start)} `} 
+                                    end={`${formatTimeFromTimestamp(schedules[0].end)} `} 
+                                    instrName={classInfo.className} 
                                     color={"green"}/>
+                            </li>
+                        ))}         
                         </div>
                     </div>
                         <div className="mt-10">
                             <h1 className="text-2xl pl-4 font-bold">การบ้านในคลาสที่ต้องส่งเร็วๆนี้</h1>
                             
-                            {DataHomework.map((item) => (
-                                <ClassHomeworkIncoming  
-                                    titleName={item.titleName} 
-                                    homeworkName={item.homeworkName} 
-                                    deadlineDayOfWeek={item.deadlineDayOfWeek} 
-                                    deadlineTime={item.deadlineTime} 
-                                    status={item.status} 
-                                    color={item.color}/>
+                            {dashboard.assignments.map(({ assignmentName, assignmentID, classInfo, submission,dueDate}) => (
+                                <li key={assignmentID} className={`flex justify-center mt-2`}>
+                                    <ClassHomeworkIncoming
+                                        titleName={assignmentName} 
+                                        homeworkName={classInfo.className} 
+                                        deadlineDayOfWeek={convertTimestampToDate(dueDate).toDateString()} 
+                                        deadlineTime={`${formatTimeFromTimestamp(dueDate)}`} 
+                                        status={submission.submissionStatus} 
+                                        />
+                                </li>
                             ))}
                         
                         </div>
                 </div>
 
-                <div className="self-start w-[500px]">
+                <div className="self-start w-[400px]">
                     <h1 className="font-bold text-2xl pb-4">ประกาศจากผู้สอน</h1>
-                    <div className="flex flex-col bg-white drop-shadow-lg pl-8 py-6">
+                    <div className="flex flex-col justify-center  bg-white drop-shadow-lg py-6">
                         <div className="flex items-center px-2 pt-2 pb-4">
                             <span className="text-xl px-1 font-semibold">ประกาศจากผู้สอน</span>
                             <span className="px-1 truncate font-semibold text-xl">classname</span>
                         </div>
-                        <div className="flex">
-                            <div className=" h-16 w-16 bg-slate-500 rounded-full mx-4">
-                                <img src="" alt="" />
-                            </div>
-                            <div className="flex flex-col w-[280px]">
-                                <span className="text-lg truncate">Name Instructor</span>
-                                <span className="text-base">โพสประกาศ วัน อ.19 ก.ย.</span>
-                                <h1 className="font-semibold truncate text-lg">headerName</h1>
-                            </div>
-                        </div>
+                        
+                        {dashboard.announcements.map(({ announcementID, courseInfo, teacher, lastEdit }) => (
+                            <li key={announcementID}  className={`flex justify-center mt-2`}>
+                                <div className="flex justify-center py-2 bg-gray-100 hover:bg-gray-300 rounded-xl">
+                                    <div className="py-2 h-16 w-16  rounded-full mx-4">
+                                        <img src={teacher.profilePic}  className=" h-16 w-16 rounded-full"/>
+                                    </div>
+                                    <div className="flex flex-col w-[280px]">
+                                        <span className="text-lg truncate">{teacher.teacherName}</span>
+                                        <span className="text-base">โพสต์ประกาศ  {convertTimestampToDate(lastEdit).toDateString()}</span>
+                                        <h1 className="font-semibold truncate text-lg">{courseInfo.courseName}</h1>
+                                    </div>
+                                </div>
+                            </li>
+                        ))}
                     </div>
                 </div>
             </div>

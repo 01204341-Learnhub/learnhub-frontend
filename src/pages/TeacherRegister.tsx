71a07a0614c696f0fee84cd2770bcfbb87734ad9
PAFFx <jj2jom@gmail.com>
2023-10-11 23:35:16 +0700

    Revert "TeacherRegister update"
    This reverts commit 43938b2f711819e6c6c322c6901f3f182b3fbca4.

--- src/pages/TeacherRegister.tsx
+++ src/pages/TeacherRegister.tsx
@@ -1,15 +1,14 @@
 import { faEnvelope, faUnlockKeyhole, faUser } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { createTeacherWithEmail } from "../services/auth/createUser";
 
-import smolRobotImage from "../../src/assets/Images/smolRobot.jpeg"
-
 function TeacherRegister() {
+    const dispatch = useDispatch();
     const navigate = useNavigate();
-    const [firstname, setFirstname] = useState('');
-    const [lastname, setLastname] = useState('');
+    const [fullname, setFullname] = useState('');
     const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
@@ -31,25 +30,16 @@ function TeacherRegister() {
         setConfirmPassword(event.target.value);
     }
 
-    const handleFirstnameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setFirstname(event.target.value)
+    const handleFullnameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setFullname(event.target.value);
     }
-    const handleLastnameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setLastname(event.target.value)
-    }
-
 
     const handleRegister = async () => {
         if (password !== confirmpassword) {
             alert("Password and Confirm Password not match");
             return
-        } else if(firstname.trim().includes(' ') || lastname.trim().includes(' ')){
-            alert("Firstname or Lastname contains whitespace");
-            return
         }
-
         try {
-            const fullname = firstname.trim() + " " + lastname.trim()
             await createTeacherWithEmail(email, password, username, fullname)
             navigate('/login', { replace: true });
         } catch (error) {
@@ -59,84 +49,70 @@ function TeacherRegister() {
 
     return (
         <div className=" bg-gray-300 h-screen">
-            <div className=" grid grid-cols-2">
-                <div className=" h-screen">
-                    <img src={smolRobotImage} alt="smolRobotImage" className=" z-0 w-full h-full relative object-cover" overflow-hidden={true}></img>
-                    <button onClick={() => { navigate('/register', { replace: true }) }} className="absolute top-6 text-[24px] text-slate-700 ml-[2%]">Back</button>
+            <div className="grid grid-cols-2">
+                <div className=" ">
+                    <button onClick={() => { navigate('/register', { replace: true }) }} className="text-[24px] ml-[2%]">Back</button>
+                    <p className="text-[24px] text-center  mt-[100px]  font-bold">**text**</p>
                 </div>
 
                 <div className="bg-white h-screen flex flex-col justify-center items-center">
                     <div className="">
                         <h1 className=" mb-[50px] text-center text-black text-[32px] font-bold">สร้างบัญชีผู้สอน</h1>
                     </div>
-                    <div className=" columns-2">
-                        <div className=" flex border-[2px] border-slate-400 rounded-xl space-x-10 object-contain h-[53px] w-[240px] items-center">
-                            <div className="  opacity-50 flex space-x-10 ml-4 w-[95%] ">
-                                <FontAwesomeIcon icon={faUser} size='2xl'></FontAwesomeIcon>
-                                <input
-                                    type="text"
-                                    className="border-black outline-none text-[24px] text-black w-full bg-white"
-                                    placeholder="ชื่อ"
-                                    value={firstname}
-                                    onChange={handleFirstnameChange}
-                                />
-                            </div>
-                        </div>
-                        <div className=" flex border-[2px]  border-slate-400 rounded-xl space-x-10 object-contain h-[53px] w-[240px] items-center">
-                            <div className="  opacity-50 flex space-x-10 ml-4 w-[95%] ">
-                                <FontAwesomeIcon icon={faUser} size='2xl'></FontAwesomeIcon>
-                                <input
-                                    type="text"
-                                    className="border-none outline-none text-[24px] text-black w-full bg-white"
-                                    placeholder="นามสกุล"
-                                    value={lastname}
-                                    onChange={handleLastnameChange}
-                                />
-                            </div>
-                        </div>
-                    </div>
-                    <div className=" mt-10 flex border-[2px]  border-slate-400 rounded-xl space-x-10 object-contain h-[53px] w-[494px] items-center">
+                    <div className=" flex border-[2px]  rounded-xl space-x-10 object-contain h-[53px] w-[494px] items-center">
                         <div className="  opacity-50 flex space-x-10 ml-4 w-[95%] ">
                             <FontAwesomeIcon icon={faUser} size='2xl'></FontAwesomeIcon>
                             <input
                                 type="text"
-                                className="border-none outline-none text-[24px] text-black w-full bg-white"
+                                className="border-none outline-none text-[24px] w-full bg-white"
+                                placeholder="ชื่อ-นามสกุล"
+                                value={fullname}
+                                onChange={handleFullnameChange}
+                            />
+                        </div>
+                    </div>
+                    <div className=" mt-10 flex border-[2px]  rounded-xl space-x-10 object-contain h-[53px] w-[494px] items-center">
+                        <div className="  opacity-50 flex space-x-10 ml-4 w-[95%] ">
+                            <FontAwesomeIcon icon={faUser} size='2xl'></FontAwesomeIcon>
+                            <input
+                                type="text"
+                                className="border-none outline-none text-[24px] w-full bg-white"
                                 placeholder="Username"
                                 value={username}
                                 onChange={handleUsernameChange}
                             />
                         </div>
                     </div>
-                    <div className=" mt-10 flex border-[2px]  border-slate-400 rounded-xl space-x-10 object-contain h-[53px] w-[494px] items-center">
+                    <div className=" mt-10 flex border-[2px] rounded-xl space-x-10 object-contain h-[53px] w-[494px] items-center">
                         <div className=" opacity-50 flex space-x-10 ml-4 w-[95%]">
                             <FontAwesomeIcon icon={faEnvelope} size='2xl'></FontAwesomeIcon>
                             <input
                                 type="email"
-                                className="border-none outline-none text-[24px] text-black w-full bg-white"
+                                className="border-none outline-none text-[24px] w-full"
                                 placeholder="Email"
                                 value={email}
                                 onChange={handleEmailChange}
                             />
                         </div>
                     </div>
-                    <div className=" mt-10 flex border-[2px]  border-slate-400 rounded-xl space-x-10 object-contain h-[53px] w-[494px] items-center">
+                    <div className=" mt-10 flex border-[2px] rounded-xl space-x-10 object-contain h-[53px] w-[494px] items-center">
                         <div className=" opacity-50 flex space-x-10 ml-4 w-[95%]">
                             <FontAwesomeIcon icon={faUnlockKeyhole} size='2xl'></FontAwesomeIcon>
                             <input
                                 type="password"
-                                className="border-none outline-none text-[24px] text-black w-full bg-white"
+                                className="border-none outline-none text-[24px] w-full "
                                 placeholder="Password"
                                 value={password}
                                 onChange={handlePasswordChange}
                             />
                         </div>
                     </div>
-                    <div className=" mt-10 flex border-[2px]  border-slate-400 rounded-xl space-x-10 object-contain h-[53px] w-[494px] items-center">
+                    <div className=" mt-10 flex border-[2px] rounded-xl space-x-10 object-contain h-[53px] w-[494px] items-center">
                         <div className=" opacity-50 flex space-x-10 ml-4 w-[95%]">
                             <FontAwesomeIcon icon={faUnlockKeyhole} size='2xl'></FontAwesomeIcon>
                             <input
                                 type="password"
-                                className="border-none outline-none text-[24px] text-black w-full bg-white"
+                                className="border-none outline-none text-[24px] w-full "
                                 placeholder="Confirm Password"
                                 value={confirmpassword}
                                 onChange={handleConfirmPasswordChange}

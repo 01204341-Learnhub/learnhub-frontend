bdf6d88beab19f613cc2cb81bfb14105a0700158
PAFFx <jj2jom@gmail.com>
2023-10-13 16:30:17 +0700

    add TeacherIncomes and assign it to /teach/incomes

---
+++ src/pages/teachers/TeacherIncomes.tsx
@@ -0,0 +1,17 @@
+import { useNavigate } from "react-router-dom";
+
+
+function TeacherIncomes(){
+    const navigate = useNavigate();
+
+    return (
+        <div className=" items-center">
+            <h1 className=" ">
+                TeacherIncomes
+            </h1>
+        </div>
+    )
+
+}
+
+export default TeacherIncomes

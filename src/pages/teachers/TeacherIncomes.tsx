a62f5412a1c76f5a45a1a698fca795f9a3a01752
PAFFx <jj2jom@gmail.com>
2023-10-13 17:00:09 +0700

    add total earnings summary

--- src/pages/teachers/TeacherIncomes.tsx
+++ src/pages/teachers/TeacherIncomes.tsx
@@ -1,14 +1,26 @@
-import { useNavigate } from "react-router-dom";
 
 
 function TeacherIncomes(){
-    const navigate = useNavigate();
 
     return (
-        <div className=" items-center">
-            <h1 className=" ">
-                TeacherIncomes
-            </h1>
+        <div className="flex bg-white h-full px-8 py-6">
+            <div className="flex flex-cols items-center w-full h-[30%]
+            px-20  bg-[#f0f0f0]">
+                <div className="flex flex-cols items-end">
+                    <div className="text-2xl text-black">
+                        ยอดเงินรวมที่ได้รับ
+                    </div>
+                    <div className="w-20 "></div>
+                    <div className="text-6xl font-bold text-black">
+                        5000
+                    </div>
+                    <div className="w-5 "></div>
+                    <div className="text-3xl font-bold text-black">
+                        บาท
+                    </div>
+                </div>
+
+            </div>
         </div>
     )
 

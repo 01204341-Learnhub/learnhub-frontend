f00c79f041c02045c423bc56bf3d1a3767ea947a
mahasachan <mahasachan.sa@ku.th>
2023-10-15 03:16:47 +0700

    add function delete attachments

--- src/pages/teachers/TeacherCourseManage.tsx
+++ src/pages/teachers/TeacherCourseManage.tsx
@@ -13,7 +13,24 @@ import { getFileNameFromSrc } from "../../utils/functions";
 import { useCourseDetail } from "../../features/stores/hooks/useCourseDetail";
 import { CourseAnnouncement } from "../../features/stores/types/courseAnnouncements";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { faStar, faTimes } from "@fortawesome/free-solid-svg-icons";
+
+function getFileType(file: File): string {
+  const imageTypes = ["image/jpeg", "image/png", "image/gif"];
+  const videoTypes = [
+    "video/mp4",
+    "video/avi",
+    "video/quicktime",
+    "video/x-ms-wmv",
+  ];
+  if (imageTypes.includes(file.type)) {
+    return "image";
+  } else if (videoTypes.includes(file.type)) {
+    return "video";
+  } else {
+    return "file";
+  }
+}
 
 function TeacherCourseManage() {
   const { id } = useParams();
@@ -82,17 +99,40 @@ function TeacherCourseManage() {
   ) => {
     const fileList = event.target.files;
     if (fileList && fileList.length == 1) {
-      let url = "";
-      const isImage = fileList[0].type.includes("image");
-      if (isImage) {
-        url = await uploadImageFile(fileList[0]);
-      } else {
-        url = await uploadFile(fileList[0]);
-      }
-      setAttachments([...attachments, { attachmentType: "file", src: url }]);
+      Swal.fire({
+        title: "กำลังอัพโหลดไฟล์",
+        timerProgressBar: true,
+        didOpen: () => {
+          Swal.showLoading();
+        },
+      })
+      const url = await uploadFile(fileList[0]);
+      setAttachments([
+        ...attachments,
+        { attachmentType: getFileType(fileList[0]), src: url },
+      ]);
+      Swal.close();
+      Swal.fire({
+        icon: "success",
+        title: "อัพโหลดไฟล์สำเร็จ",
+        showConfirmButton: false,
+        timer: 1500,
+      })
     } else {
       alert("กรุณาเลือกไฟล์เพียง 1 ไฟล์");
     }
+  };
+
+  const handleDeleteAttachment = (index: number) => {
+    const newAttachments = attachments.filter((attachment, i) => i !== index);
+    newAttachments.slice(index, 1);
+    setAttachments(newAttachments);
+    Swal.fire({
+        icon: "success",
+        title: "ลบไฟล์สำเร็จ",
+        showConfirmButton: false,
+        timer: 1500,
+    })
   };
 
   const addNewAnnouncement = async (): Promise<string> => {
@@ -130,14 +170,60 @@ function TeacherCourseManage() {
     setIsClickAdd(false);
   };
 
+  const renderPreviewAttachments = (type: string, src: string) => {
+    if (type == "image") {
+      return (
+          <img src={src} alt="" className=" object-cover w-full h-full" />
+      );
+    } else if (type == "video") {
+      return (
+          <video src={src} className=" object-cover w-full h-full" />
+      );
+    } else if (type == "file") {
+      return (
+          <p>file</p>
+      );
+    }
+  };
+
+  const renderAttachments = () => {
+    return (
+      <>
+        {attachments.map((attach, index) => {
+          return (
+            <div key={index} className="flex items-center justify-center mt-4">
+              <a className="w-11/12 h-16 border-2 flex">
+                <div className="w-1/5 border-r-2">
+                  {renderPreviewAttachments(attach.attachmentType, attach.src)}
+                </div>
+                <div className="w-4/5 h-full flex flex-col items-center justify-center mx-2 overflow-hidden">
+                  <a href={attach.src}>
+                    <h1 className="text-xs text-[#606060] truncate">
+                      {attach.src}
+                    </h1>
+                  </a>
+                </div>
+              </a>
+              <button className="mx-2"
+                onClick={() => handleDeleteAttachment(index)}
+              >
+                <FontAwesomeIcon icon={faTimes} size="lg"></FontAwesomeIcon>
+              </button>
+            </div>
+          );
+        })}
+      </>
+    );
+  };
+
   const AddAnnouncementModal = () => {
     return (
       <div className="relative z-10">
         <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
         <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
           <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-            <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-              <div className="bg-white px-6 pb-4 pt-5">
+            <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all w-1/3">
+              <div className="bg-white px-8 pb-4 pt-5">
                 <div className="mt-3 text-center flex flex-col items-start">
                   <h3
                     className="text-base font-semibold leading-6 self-center text-gray-900"
@@ -173,34 +259,7 @@ function TeacherCourseManage() {
                     id="textAnc"
                   />
                 </div>
-
-                {attachments.length != 0 ? (
-                  <>
-                    {attachments.map((attach, index) => {
-                      return (
-                        <a
-                          key={index}
-                          className="w-full mt-4 h-16 border-2 flex"
-                        >
-                          <div className="w-1/5 border-r-2">
-                            <img
-                              src={attach.src}
-                              alt=""
-                              className=" object-cover w-full h-full"
-                            />
-                          </div>
-                          <div className="w-4/5 h-full flex flex-col items-center justify-center mx-2 overflow-hidden">
-                            <a href={attach.src}>
-                              <h1 className="text-xs text-[#606060] truncate">
-                                {attach.src}
-                              </h1>
-                            </a>
-                          </div>
-                        </a>
-                      );
-                    })}
-                  </>
-                ) : null}
+                {renderAttachments()}
               </div>
               <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse justify-between items-center sm:px-6">
                 <div className="flex flex-row-reverse">
@@ -408,10 +467,7 @@ function TeacherCourseManage() {
               <h1 className="text-lg font-semibold">
                 โพสต์ที่ประกาศในคอร์สนี้
               </h1>
-              <button
-                onClick={handleAddAnnouncement}
-                className="py-1"
-              >
+              <button onClick={handleAddAnnouncement} className="py-1">
                 + สร้างโพสต์
               </button>
             </div>

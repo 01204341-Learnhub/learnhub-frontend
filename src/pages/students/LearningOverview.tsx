9515649c78c9d429148cdc1ca41c541043ec3650
mahasachan <mahasachan.sa@ku.th>
2023-10-16 13:15:47 +0700

    fix duetime

--- src/pages/students/LearningOverview.tsx
+++ src/pages/students/LearningOverview.tsx
@@ -79,7 +79,7 @@ function LearningOverview() {
         return teachingClass
     }
     function formatTimeFromTimestamp(timestamp: number): string {
-        const date = new Date(timestamp);
+        const date = new Date(timestamp*1000);
         const hours = date.getHours().toString().padStart(2, '0');
         const minutes = date.getMinutes().toString().padStart(2, '0');
         return `${hours}:${minutes}`;

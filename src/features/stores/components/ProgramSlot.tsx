abfdde4b4ff1bdfa02705c78b09bbfbd8e6f6ce2
mahasachan <mahasachan.sa@ku.th>
2023-11-04 13:55:28 +0700

    modify layout card

--- src/features/stores/components/ProgramSlot.tsx
+++ src/features/stores/components/ProgramSlot.tsx
@@ -18,16 +18,15 @@ interface ProgramSlotProps {
 
 function ProgramSlot(myProgram: ProgramSlotProps) {
     return (
-        <div className="bg-white rounded-[10px] w-[286px] h-[300px] overflow-hidden shadow-lg ">
-            <div className=" w-[286px] h-[130px]">
+        <div className="bg-white rounded-[10px] w-[286px] h-[355px] overflow-hidden shadow-lg">
+            <div className=" w-[286px] h-[160px]">
                 <img className="w-full h-full object-cover" src={myProgram.courseThumbnailUrl} alt="Larm On Chang" />
-
             </div>
-            <div className="px-5 h-[150px] flex flex-col">
-                <div className=" font-extrabold text-[16px] mb-2">{myProgram.courseName}</div>
+            <div className="px-5 h-[180px] flex flex-col">
+                <div className=" font-extrabold text-[16px] mb-2 text-ellipsis line-clamp-3">{myProgram.courseName}</div>
                 <p className="-mt-3 font-bold text-gray-500 text-[14px]">{myProgram.instructorName}</p>
                 {myProgram.percentCompleted !== null && myProgram.voter !== null ?
-                    <div className=" flex items-center grow space-x-1 text-[14px] font-bold">{myProgram.rating}
+                    <div className=" flex grow space-x-1 text-[14px] font-bold">{myProgram.rating}
                         <div className="pl-1">
                             {[1, 2, 3, 4, 5].map((index) => (
                                 <FontAwesomeIcon
@@ -49,6 +48,7 @@ function ProgramSlot(myProgram: ProgramSlotProps) {
                         <div className="  p-2 font-semibold text-[14px] text-gray-500 text-center " >{myProgram.lvl}</div>
                     </div>
                 </div>
+                <div className="h-2"></div>
             </div>
         </div>
     )

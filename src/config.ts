ae4e9c5f3f72d9d50e45370d6c664a47ac9f01bf
Nhuengzii <32nhueng@gmail.com>
2023-11-05 13:51:15 +0700

    update: base_url

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 // const BASE_URL = import.meta.env.VITE_BASE_API_URL || "http://localhost:8000";
-const BASE_URL = "http://10.31.73.253:8000";
+const BASE_URL = "http://172.20.10.2:8000";
 export { BASE_URL };
